b5b41bdfb33840e4d60be9faa65ee5ae347112ff
Marcos Vinícius Silva <marvinc.silva@gmail.com>
2016-12-18 20:24:30 -0200

    adds a default command as help

--- sat.js
+++ sat.js
@@ -1,8 +1,15 @@
 const package = require('./package.json')
 const program = require('commander')
+const colors = require('colors')
 
 require('./usage')(program)
 
 program.version(package.version)
   .usage('<command>')
+  .on('*', () => {
+    const help = `Cannot run '${colors.red('sat', program.args[0])}' try '${colors.green('sat --help')}' to get the following help`
+
+    console.log(help)
+    program.help()
+  })
   .parse(process.argv)
